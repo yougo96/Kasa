0cfa26dbffec067bc77b31ac1524c77885fde851
HP <31686938+yougo96@users.noreply.github.com>
2024-08-01 21:05:48 +0200

    Gallery home

---
+++ src/components/rentalCard.jsx
@@ -0,0 +1,10 @@
+export function RentalCard ({id, title, src})   {
+    
+    return (
+       <article id={id} onClick={() => window.location.href = `/rental/${id}`}>
+        <img src={src} alt={title} />
+        <div className="rental-filter"></div>
+        <h2>{title}</h2>
+       </article>
+    )
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,19 @@
 import { Banner } from "../components/banner"
+import { RentalCard } from "../components/rentalCard"
+
+import rentalData from '../assets/rental.json';
 
 export function Home ()   {
    
     return (
         <>
             <Banner src="/bannerHome.png">Chez vous, partout et ailleurs</Banner>
+            <div className="home-container">
+                {rentalData.map((data, index) => (
+                    // <div key={index}>{data.title}</div>
+                    <RentalCard key={index} id={data.id} title={data.title} src={data.cover} />
+                ))}
+            </div>
         </>
     )
 }
