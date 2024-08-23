fea13259ab486689b216d9bb6c1ff6054619e778
HP <31686938+yougo96@users.noreply.github.com>
2024-08-23 18:34:28 +0200

    Animation collapse

--- src/components/collapse.jsx
+++ src/components/collapse.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react"
 import './collapse.scss'
 
+
 export function Collapse ({title, children})   {
+
+    const [open, setOpen] = useState(false)
     
      return (
-        <details>
+        <div id="details" onClick={(e) => setOpen(!open)} open={open}>
             <summary>{title}</summary>
             {children && <div className="details-content">{children}</div> }
-        </details>
+        </div>
      )
 }
