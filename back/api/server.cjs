f4415f987762b7daeaffa7eba5451f8dbfdfc9ed
HP <31686938+yougo96@users.noreply.github.com>
2024-10-21 17:27:20 +0200

    docker

--- back/api/server.cjs
+++ back/api/server.cjs
@@ -18,10 +18,9 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+// server.use(jsonServer.rewriter({
+//     '/api/*': '/$1'
+// }))
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
